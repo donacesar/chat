045f9749e89fef84add1f4c1ce5594ec656b5c70
donacesar <donacesar@gmail.com>
2021-11-25 11:50:29 +0300

    Добавил интервалбную проверку связи с сервером

--- script/script.js
+++ script/script.js
@@ -1,37 +1,43 @@
+/* Клиент чата */
 
-//блок для вывода информации на экран
+// Блок для вывода информации на экран
 function message(text, code) {
 	text = '<div>' + text + '</div>';
 	//выводим в chat-result код состояния и текст сообщения 
-	jQuery('#chat-result').append("Код состояния" + code);
+	jQuery('#chat-result').append("Код состояния : " + code);
 	jQuery('#chat-result').append(text);
+}
+
+// Проверка соединения с сервером 
+function testConnection() {
+	JQuery('#chat-result').append('### : ', socket.readyStatec);
 }
 
 
 
 jQuery(document).ready(function($) {
 
-	//Открываем соединение с сервером
+	// Открываем соединение с сервером
 	/* У сокета есть 4 коллбека (описаны ниже) */
 	let socket = new WebSocket("ws://192.168.2.165:8090/server.php");
 
-	//Событие при соединении с сервером
+	// Событие при соединении с сервером
 	socket.onopen = function() {
 		message("[open] Соединение установлено.", socket.readyState);
 	};
 
-	//Событие срабатывает при ошибке соединения с сервером
+	// Событие срабатывает при ошибке соединения с сервером
 	socket.onerror = function(error) {
 		message("[error] Ошибка соединения с сервером. " +  (event.code ? "Код = " + event.code + " " + error.massage : ""), socket.readyState);
 	};
 
-	//Событие срабатывает при закрытии соединения
+	// Событие срабатывает при закрытии соединения
 	socket.onclose = function() {
 		if (event.wasClean) {
 			//если соединение закрыто чисто : 
 			message(`[close] Соединение закрыто. Код = ${event.code} причина = ${event.reason}`, socket.readyState);
 		} else {
-			// например, сервер убил процесс или сеть недоступна
+			// например, сервер убил процесс или сеть недоступна(когда TCP протокол вернет ошибку)
 			// обычно в этом случае event.code 1006
 			message(`[close] Соединение прервано. Код = ${event.code}`, socket.readyState);
 		}
@@ -60,4 +66,6 @@ jQuery(document).ready(function($) {
 			socket.send(JSON.stringify(message));
 			return false;
 	});
+
+	setInterval(testConnection(), 100000);
 });
