545915bdb1fd1d21e478426286f87619adb2ab66
donacesar <donacesar@gmail.com>
2021-12-02 13:09:53 +0300

    поставил интервал для recv3

--- server.php
+++ server.php
@@ -87,7 +87,7 @@ while(true) {
         $dataSize = 0;
         while (true) {
             $recvBytes = 0;
-            $recvBytes = socket_recv($newSocketArrayResource, $socketData, 1024, MSG_OOB);
+            $recvBytes = socket_recv($newSocketArrayResource, $socketData, 1024, 0);
             echo $recvBytes . "\n";
             if ($recvBytes == 0) {
                 break;
@@ -102,10 +102,8 @@ while(true) {
 
 
 
-        echo "\nПришло $dataSize байт\nПкрвые 8 байт";
-        $unpackArr = unpack('h8', $socketData);
-        print_r($unpackArr);
-
+        echo "\nПришло $dataSize байт";
+        
         // Пока здесь while (планировался цикл для чтения нескольких фреймов)
         while($dataSize) {
 
